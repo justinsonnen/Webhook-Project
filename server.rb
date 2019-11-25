4e766ce98acb2745a6387009ce079687616a3b4f
Justin Sonnen <36781953+justinsonnen@users.noreply.github.com>
2019-11-25 12:35:37 -0700

    changing the output

--- server.rb
+++ server.rb
@@ -54,7 +54,7 @@ class GHAapp < Sinatra::Application
   
   # TEST
     # see if we can print the wildcard 
-    puts request.env['HTTP_X_GITHUB_EVENT']
+    logger.debug "Whats up Sucka---- received event #{request.env['HTTP_X_GITHUB_EVENT']}"
     200 # success status
   end
 
